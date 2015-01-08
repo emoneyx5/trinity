ca7774060edef70b0d40d94f358ade51b505e72a
edwin kim <emoney86@gmail.com>
2015-01-08 16:15:04 -0500

    add signout link if signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#main'
   get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
